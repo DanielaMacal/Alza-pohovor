cce01b825aee93dfc3b231b670e95a7fd1609440
Daniela Máčalová <dan.macalova@gmail.com>
2021-11-21 17:10:30 +0100

    JS complete

--- index.js
+++ index.js
@@ -10,20 +10,44 @@ function myFunction2() {
   <h4>Průměrná roční úspora člena Alza Premium</h4>
   `}
 
-function myFunction3() {
-  document.getElementById("column2").style.display = "flex"
-  document.getElementById("column3").style.display = "none"
-  document.getElementById("column4").style.display = "none"
-}
+  function myFunction3() {
+    myReset();
+    myReset2();
+    document.getElementById("column2").classList.add('show');
+    document.getElementById("column3").classList.add('hide');
+    document.getElementById("column4").classList.add('hide');
+    document.getElementById("choice1").classList.add('active');
+  }
+  
+  function myFunction4() {
+    myReset();
+    myReset2();
+    document.getElementById("column2").classList.add('hide');
+    document.getElementById("column3").classList.add('show');
+    document.getElementById("column4").classList.add('hide');
+    document.getElementById("choice2").classList.add('active');
+  }
+  
+  function myFunction5() {
+    myReset();
+    myReset2();
+    document.getElementById("column2").classList.add('hide');
+    document.getElementById("column3").classList.add('hide');
+    document.getElementById("column4").classList.add('show');
+    document.getElementById("choice3").classList.add('active');
+  }
+  
+  function myReset() {
+    document.getElementById("column2").classList.remove('hide');
+    document.getElementById("column2").classList.remove('show');
+    document.getElementById("column3").classList.remove('hide');
+    document.getElementById("column3").classList.remove('show');
+    document.getElementById("column4").classList.remove('hide');
+    document.getElementById("column4").classList.remove('show');
+  }
 
-function myFunction4() {
-  document.getElementById("column2").style.display = "none"
-  document.getElementById("column3").style.display = "flex"
-  document.getElementById("column4").style.display = "none"
-}
-
-function myFunction5() {
-  document.getElementById("column2").style.display = "none"
-  document.getElementById("column3").style.display = "none"
-  document.getElementById("column4").style.display = "flex"
-}
+  function myReset2() {
+    document.getElementById("choice1").classList.remove('active');
+    document.getElementById("choice2").classList.remove('active');
+    document.getElementById("choice3").classList.remove('active');
+  }
